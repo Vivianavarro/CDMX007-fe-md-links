a3c77eaf817c712314d563d9c8f0c48723f7a9ad
Viviana Navarro <nitoname@gmail.com>
2019-04-05 09:00:19 -0600

    actualización de modulo comand

--- comand.js
+++ comand.js
@@ -1,27 +1,2 @@
 const index = require('./app');
 const links = require('./matchLink');
-const infoRequired = process.argv.splice(2);
-const validate = infoRequired.includes('--validate');
-const stats = infoRequired.includes('--stats');
-
-if(infoRequired.length > 0) {
-    if(infoRequired[0] !== '--validate' && infoRequired[0] !== '--stats') {
-        index.lookFor(infoRequired[0]);
-        
-    } else {
-        index.lookFor('./');
-    }
-  };
-
-  const typeOfValidation = (linksArray, file, Path) => {
-    if(validate && stats) {
-        links.countigValidatedLinks(linksArray, file, Path);
-    } else if (stats){
-        links.counting(linksArray, file, Path);
-    } else if (validate) {
-        links.validatingLinks(linksArray, file, Path);
-        
-    }
-  }
-
-  module.exports.typeOfValidation = typeOfValidation;
